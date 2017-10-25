fa2bda190658dc350c66a1299fef2b67ad957666
yaw owusu <asirifiyaw@gmail.com>
2017-10-25 11:26:39 -0700

    updating the table after an order

--- bama.js
+++ bama.js
@@ -18,7 +18,7 @@ connection.connect(function(err){
 });
 
 function nowConnectedToProducts(){
-	// 	connection.query("select `item_id` `id`, `product_name` `Product`, `department_name` `Department`, `price` `Price($)`, `stock_quantity` `Quantity` from products", function(err, response){
+	// 	connection.query("select `item_id` `id`, `product_name` `Product`, `department_name` `Department`, `price` `Dollars`, `stock_quantity` `Quantity` from products", function(err, response){
 	// 	if(err){throw err};
 	// 	//console.log(response);
 	// 	// for(var i=0; i<response.length; i++){ 
@@ -27,7 +27,7 @@ function nowConnectedToProducts(){
 	// 	console.table(response);
 	// });
 
-	connection.query("select `item_id` `id`, `product_name` `Product`, `price` `Price($)` from products", function(err, response){
+	connection.query("select `item_id` `id`, `product_name` `Product`, `price` `Dollars` from products", function(err, response){
 	if(err){throw err};
 	console.table("Available Items \n",response);
 	promptUser();
@@ -57,26 +57,21 @@ function nowConnectedToProducts(){
 
 	
 		console.log("\nUpdating Products Table ...\n");
-		connection.query(
-			"UPDATE products SET ? WHERE ?",
-			[
-				{
-					item_id: 1
-				},
-				{
-					stock_quantity: 1000
-				}
-			],function(err, res){
-				console.log(res);
-			}
-		)
-	});
+		// connection.query("select `item_id` `id`, `product_name` `Product`, `price` `Dollars`, `stock_quantity` `Level` from products",function(err, res){
+		// 	if(err){throw err};
+		// 	//console.log(res); shows raw response with Alias names
+		// 		console.log(res[0].id);
+		// 		console.log(res[0].Product);
+		// 		console.log(res[0].Dollars);
+		// 		console.log(res[0].Level);
+		// 	});
+	
+		connection.query("UPDATE products SET ? WHERE ?", [{ stock_quantity: 22}, {item_id: 1 }], 
+			function(err, res) {
+      		console.log(res.affectedRows + " products updated!\n");
+      		})
 
-}
-
-
-}
-//promptUser();
-
-   
-
+			
+		});
+	};
+};
