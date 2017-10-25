8ebf3169fab3c00af43fecced25b2d138a450d6c
yaw owusu <asirifiyaw@gmail.com>
2017-10-25 08:09:31 -0700

    adding prompts

--- bama.js
+++ bama.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var consoleTable = require("console.table"); //needed to log the product table 
+var prompt = require("prompt");
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -15,18 +16,67 @@ connection.connect(function(err){
 	console.log("connected as id "+ connection.threadId + "\n");
 	nowConnectedToProducts();
 });
-function nowConnectedToProducts(){
-		connection.query("select `item_id` `id`, `product_name` `Product`, `department_name` `Department`, `price` `Price($)`, `stock_quantity` `Quantity` from products", function(err, response){
-		if(err){
-			throw err;
-		}
 
-		//console.log(response);
-		// for(var i=0; i<response.length; i++){  // organizing response nicely
-		// 	console.log(response[i].item_id + "|" + response[i].product_name + "|" 
-		// 	+ response[i].department_name + "|" + response[i].price + "|" + response[i].stock_quantity);
-		// }
-		console.table(response);
+function nowConnectedToProducts(){
+	// 	connection.query("select `item_id` `id`, `product_name` `Product`, `department_name` `Department`, `price` `Price($)`, `stock_quantity` `Quantity` from products", function(err, response){
+	// 	if(err){throw err};
+	// 	//console.log(response);
+	// 	// for(var i=0; i<response.length; i++){ 
+	// 	// 	console.log(response[i].item_id + "|" + response[i].product_name + "|" 
+	// 	// 	+ response[i].department_name + "|" + response[i].price + "|" + response[i].stock_quantity);}
+	// 	console.table(response);
+	// });
+
+	connection.query("select `item_id` `id`, `product_name` `Product`, `price` `Price($)` from products", function(err, response){
+	if(err){throw err};
+	console.table("Available Items \n",response);
+	promptUser();
+		
+	});
+	prompt.start();
+	var promptUser = function(){
+	//var self = this;
+	
+	// Get order and quantity 
+	prompt.get([{name:'item_id',
+				description: 'Product_Id',
+				type: 'integer',
+				required: true
+				},
+				{name:'stock_quantity',
+				description: 'Order Quantity',
+				type: 'integer',
+				required: true}], function (err, result) {
+
+
+		console.log('\nOrder Details:');
+		console.log('~~~~~~~~~~~~~');
+		console.log('Item Id: ' + result.item_id);
+		console.log('Quantity: ' + result.stock_quantity);
+		console.log('~~~~~~~~~~~');
+
+	
+		console.log("\nUpdating Products Table ...\n");
+		connection.query(
+			"UPDATE products SET ? WHERE ?",
+			[
+				{
+					item_id: 1
+				},
+				{
+					stock_quantity: 1000
+				}
+			],function(err, res){
+				console.log(res);
+			}
+		)
 	});
 
 }
+
+
+}
+//promptUser();
+
+   
+
